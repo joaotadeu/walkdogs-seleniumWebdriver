f112cd5c19c9dae4ba1a15902234737519401b02
João Tadeu Soares Pereira <ojoaotadeu@gmail.com>
2024-04-20 19:27:49 -0300

    adicionando implementação de preenchimentos de dados de parceiro atraves do datatable

--- src/test/java/br/com/joaotadeu/steps/Cadastro_parceiro_walkdog_steps.java
+++ src/test/java/br/com/joaotadeu/steps/Cadastro_parceiro_walkdog_steps.java
@@ -36,11 +36,18 @@ public class Cadastro_parceiro_walkdog_steps {
         WebElement nomeInput = navegador.findElement(By.name("name")); // Supondo que o ID do campo de nome seja "name"
         WebElement emailInput = navegador.findElement(By.name("email")); // Supondo que o ID do campo de email seja "email"
         WebElement cpfInput = navegador.findElement(By.name("cpf")); // Supondo que o ID do campos de CPF seja "CPF"
+        WebElement cepInput = navegador.findElement(By.name("cep"));
+        WebElement numeroInput = navegador.findElement(By.name("addressNumber"));
+        WebElement complementoInput = navegador.findElement(By.name("addressDetails"));
 
         // Preenchendo os campos com os dados do parceiro
         nomeInput.sendKeys(dadosParceiro.get(0).get("Nome Completo"));
         emailInput.sendKeys(dadosParceiro.get(0).get("E-mail"));
         cpfInput.sendKeys(dadosParceiro.get(0).get("CPF"));
+        cepInput.sendKeys(dadosParceiro.get(0).get("CEP"));
+        navegador.findElement(By.cssSelector("#page-walker > form:nth-child(2) > fieldset:nth-child(4) > div:nth-child(2) > div:nth-child(2) > input:nth-child(1)")).click();
+        numeroInput.sendKeys(dadosParceiro.get(0).get("Numero"));
+        complementoInput.sendKeys(dadosParceiro.get(0).get("Complemento"));
     }
     @Quando("escolho atividade extra")
     public void escolho_atividade_extra(io.cucumber.datatable.DataTable dataTable) {
