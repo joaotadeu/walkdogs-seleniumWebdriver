9da4cf446f71583356f5e8af5c0007364af4a4f8
João Tadeu Soares Pereira <ojoaotadeu@gmail.com>
2024-05-08 18:27:06 -0300

    adicionando pdrão DriverFactory ao projeto, centralizando a inicialiação do driver

--- src/test/java/br/com/joaotadeu/steps/steps_starbugs/starbugs_steps.java
+++ src/test/java/br/com/joaotadeu/steps/steps_starbugs/starbugs_steps.java
@@ -1,5 +1,6 @@
 package br.com.joaotadeu.steps.steps_starbugs;
 
+import br.com.joaotadeu.DriverFactory.DriverFactory;
 import io.cucumber.datatable.DataTable;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
@@ -8,11 +9,9 @@ import io.cucumber.java.pt.Dado;
 import io.cucumber.java.pt.E;
 import io.cucumber.java.pt.Então;
 import io.cucumber.java.pt.Quando;
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.apache.commons.io.FileUtils;
 import org.junit.Assert;
 import org.openqa.selenium.*;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -31,9 +30,7 @@ public class starbugs_steps {
 
     @Before
     public void setUpStarbugsStarbugs(){
-        // Configuração do navegador
-        WebDriverManager.firefoxdriver().setup();
-        navegadorStarbugs = new FirefoxDriver();
+        navegadorStarbugs = DriverFactory.getDriver();
         wait = new WebDriverWait(navegadorStarbugs, Duration.ofSeconds(10));
         System.out.println("Iniciando Teste...");
     }
