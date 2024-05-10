21872fe747666a0ec718f922606e4591f2acf372
João Tadeu Soares Pereira <ojoaotadeu@gmail.com>
2024-05-10 18:33:32 -0300

    finalizando padrão pageobjects no projeto Walkdogs

--- src/test/java/br/com/joaotadeu/runners/Runner.java
+++ src/test/java/br/com/joaotadeu/runners/Runner.java
@@ -8,7 +8,7 @@ import io.cucumber.junit.CucumberOptions;
 @CucumberOptions(
         features = "src/test/java/br/com/joaotadeu/features/Walkdog.feature",
         glue = "br.com.joaotadeu.steps.steps_walkdogs",
-        plugin = {"pretty", "html: logs/report-html", "json: logs/report.json"},
+        plugin = {"pretty", "html: logs/report-html", "json: logs/report.json","json:target/cucumber.json","junit:target/cucumber.xml"},
         tags = "@walkdog"
 )
 public class Runner {
